d73b13d3bbf5a3b303940dd9b44729937a846b20
tuanta <tuantran.kum@gmail.com>
2023-01-29 15:41:45 +0700

    7 frontend - ONCLICK SHOW MODAL-2

--- src/components/CreatePostModel/index.js
+++ src/components/CreatePostModel/index.js
@@ -1,19 +1,33 @@
 import { Modal } from '@mui/material'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { hideModal } from '../../redux/action';
 import { selectShowModal } from '../../redux/selector'
 
 export default function CreatePostModel() {
 
-    const showModal = useSelector(selectShowModal);
+    const dispatch = useDispatch();
 
-    const body = <p>this is body modal</p>
+    const showModal = useSelector(selectShowModal);
+    const handleClose = () => {
+        dispatch(hideModal());
+    }
+
+
+    const bodyModal = (<div className=''>
+
+    </div>)
 
     // RENDER
     return (
         <div>
-            <Modal open={showModal}>
-                {body}
+            <Modal 
+                open={showModal}
+                onClose={handleClose}
+                aria-labelledby="parent-modal-title"
+                aria-describedby="parent-modal-description"
+            >
+                {bodyModal}
             </Modal>
         </div>
     )
